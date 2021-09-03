bd9c730d49ae8b37e1b137e2dfdc703103dd5c74
Labuser <labuser@Labusers-MacBook-Air.local>
2021-09-03 19:05:25 +0300

    A* done

--- assignment_1/submission.py
+++ assignment_1/submission.py
@@ -289,7 +289,9 @@ def euclidean_dist_heuristic(graph, v, goal):
     # TODO: finish this function!
     goal_pos = graph.nodes[goal]['pos']
     v_pos = graph.nodes[v]['pos']
-    return ((goal_pos[0]-v_pos[0])**2 + (goal_pos[1]-v_pos[1])**2)**0.5
+    h = ((goal_pos[0]-v_pos[0])**2 + (goal_pos[1]-v_pos[1])**2)**0.5
+    # print("h for {} is {}".format(v,h))
+    return h
     raise NotImplementedError
 
 
@@ -320,37 +322,47 @@ def a_star(graph, start, goal, heuristic=euclidean_dist_heuristic):
     found_path = False
     branch = {}
     
-    while frontier:
-        current_cost, _ , current_node = frontier.pop() 
+    while frontier.size():
+        _, _ , current_node = frontier.pop() 
+        if current_node == start:
+            current_cost = 0.0
+        else:
+            current_cost = branch[current_node][0]
+        # print(current_node)
         if current_node == goal:   
             found_path = True
+            break
         
         explored.add(current_node)
         # print(graph[current_node])
         for neighbour in sorted(graph.neighbors(current_node)): #the queue is structured as (priority, counter,node)
             neighbour_cost = graph.get_edge_weight(current_node,neighbour)
             cost_total = current_cost + neighbour_cost
-            h = euclidean_dist_heuristic (graph,neighbour,goal)
+            # print("cost for {} is: {}".format(neighbour,cost_total))
+            h = heuristic(graph,neighbour,goal)
             f = cost_total + h 
-            if neighbour not in frontier and neighbour not in explored:
+            # print(f)
+            if  neighbour not in frontier and neighbour not in explored:
                 frontier.append((f, neighbour))
                 branch [neighbour] = (cost_total, current_node) #add the parent branch
                 
-            elif neighbour in frontier and f < (branch[neighbour][0]+h):
+            elif neighbour in frontier and cost_total < branch[neighbour][0]:
                 #how to remove while not knowing the counter number?
                 frontier.append((f,neighbour))#is it okay to add without removing
                 branch [neighbour] = (cost_total, current_node) #add the parent branch 
             
-            if found_path:
-                n = goal
-                path = []
-                path.append(goal)
-                while branch[n][1] != start:
-                    path.append(branch[n][1])
-                    n = branch[n][1]
-                path.append(branch[n][1]) #append the start
-                path.reverse()
-                return path
+    if found_path:
+        # print(len(explored))
+        n = goal
+        path = []
+        path.append(goal)
+        while branch[n][1] != start:
+            path.append(branch[n][1])
+            n = branch[n][1]
+        path.append(branch[n][1]) #append the start
+        path.reverse()
+        return path
+                
 
         
 
